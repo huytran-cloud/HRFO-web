823505f9865509ebb190ffb37b5ae311b2f29162
Supporter09 <maivannhatminh2005@gmail.com>
2021-07-18 21:11:23 +0700

    change home to react component

---
+++ hrfo_web/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+import './css/base.css'
+import './css/vendor.css'
+import './css/main.css'
+
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  );
+}
+
+export default App;
